98111aca653fa477c1b7bd77e95868ae05b5f532
islomabdullaev1 <islom.abdullaev.02@mail.ru>
2025-07-02 16:53:08 +0500

    everything is working

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, World! This is a test deployment and updated !")
+    return HttpResponse("Hello, World! This is a test deployment and updated ! EVERYTHIN IS OK !")
