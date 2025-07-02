5103b89aa276a11f8806cb99342622dfd6e2804a
islomabdullaev1 <islom.abdullaev.02@mail.ru>
2025-07-02 16:46:35 +0500

    test added in ci cd

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HomePageTests(TestCase):
+    def test_home_page_status_code(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_contains_correct_html(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'Hello, World!')
