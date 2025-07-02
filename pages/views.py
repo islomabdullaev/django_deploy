a15a49de21f193da9e9b5aae8c301883eb197db8
islomabdullaev1 <islom.abdullaev.02@mail.ru>
2025-07-02 17:10:03 +0500

    runner with self host

--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello, World! This is a test deployment and updated ! EVERYTHIN IS OK !")
+    return HttpResponse("Hello, World! Runned with self hosted runner")
